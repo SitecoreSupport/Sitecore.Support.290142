5dbe6321a3f40b7ec9c99522d54195195eccc19f
iro <iro@sitecore.net>
2018-11-01 18:06:37 +0200

    Initial

---
+++ src/Sitecore.Support.290142/SaveCompositeItems.cs
@@ -0,0 +1,17 @@
+﻿namespace Sitecore.Support.XA.Feature.Composites.EventHandlers
+{
+  using Sitecore.Data.Items;
+  using Sitecore.Pipelines;
+  using Sitecore.Pipelines.ResolveRenderingDatasource;
+
+  public class SaveCompositeItems : Sitecore.XA.Feature.Composites.EventHandlers.SaveCompositeItems
+  {
+    protected override Item GetCompositeDatasource(Item contextItem, string datasource)
+    {
+      ResolveRenderingDatasourceArgs resolveRenderingDatasourceArgs = new ResolveRenderingDatasourceArgs(datasource);
+      resolveRenderingDatasourceArgs.CustomData["contextItem"] = contextItem;
+      CorePipeline.Run("resolveRenderingDatasource", resolveRenderingDatasourceArgs, false);
+      return contextItem.Database.GetItem(resolveRenderingDatasourceArgs.Datasource);
+    }
+  }
+}
